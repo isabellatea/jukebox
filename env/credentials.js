3fcb1f46202870f493bab90b70a8f48de5a685fb
Steve Rodriguez <silentcf@gmail.com>
2017-10-17 19:27:15 -0500

    update

---
+++ env/credentials.js
@@ -0,0 +1,4 @@
+exports.client_id = '2e0028dbc00344468b73c6ce68ed5469';
+exports.client_secret ='6041e2803b7146c79980fb7d808a7058';
+exports.redirect_uri = 'https://jukebox-pizza.herokuapp.com';
+exports.prod = false;
